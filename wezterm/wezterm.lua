9dadd5259c89695b005c81edcc2bc5a72ebe3559
Dangerise <2237556709@qq.com>
2024-04-14 16:31:57 +0800

    add wezterm and i3

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require "wezterm"
+
+local config = wezterm.config_builder()
+
+config.default_prog = { "/usr/bin/fish" }
+
+config.keys = {
+    { -- 控制左右移动面板
+        key = 'h',
+        mods = 'ALT',
+        action = wezterm.action {
+            ActivateTabRelative = -1
+        }
+    },
+    {
+        key = 'l',
+        mods = 'ALT',
+        action = wezterm.action {
+            ActivateTabRelative = 1
+        }
+    },
+    { -- Delete Tab
+        key = 'w',
+        mods = 'ALT',
+        action = wezterm.action.CloseCurrentTab {
+            confirm = true
+        }
+    },
+    {
+        -- New Tab
+        key = "n",
+        mods = "ALT",
+        action = wezterm.action.SpawnTab 'DefaultDomain'
+    }
+}
+
+return config
